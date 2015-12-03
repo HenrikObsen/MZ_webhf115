1e2f479322cb50def2ddb91443a281bf908e4f87
HenrikObsen <hto@djes.dk>
2015-12-03 13:55:34 +0100

    Commit 1

---
+++ MZ_webhf115/Areas/Admin/Content/ddMenu/ddmenu.js
@@ -0,0 +1,93 @@
+﻿
+        $(".submenu").hide(function () {
+        });
+
+$(".menuli").hover(function () {
+    $(".submenu").slideDown();
+},
+function () {
+    $(".submenu").hide();
+});
+
+$(".submenu").hover(function () {
+    $(".submenu").show();
+},
+function () {
+    $(".submenu").hide();
+});
+/*--------------*/
+        
+$(".submenu2").hide(function () {
+});
+
+$(".menuli2").hover(function () {
+    $(".submenu2").slideDown();
+},
+function () {
+    $(".submenu2").hide();
+});
+
+$(".submenu2").hover(function () {
+    $(".submenu2").show();
+},
+function () {
+    $(".submenu2").hide();
+});
+
+/*--------------*/
+
+$(".submenu3").hide(function () {
+});
+
+$(".menuli3").hover(function () {
+    $(".submenu3").slideDown();
+},
+function () {
+    $(".submenu3").hide();
+});
+
+$(".submenu3").hover(function () {
+    $(".submenu3").show();
+},
+function () {
+    $(".submenu3").hide();
+});
+
+
+/*--------------*/
+
+$(".submenu4").hide(function () {
+});
+
+$(".menuli4").hover(function () {
+    $(".submenu4").slideDown();
+},
+function () {
+    $(".submenu4").hide();
+});
+
+$(".submenu4").hover(function () {
+    $(".submenu4").show();
+},
+function () {
+    $(".submenu4").hide();
+});
+
+/*--------------*/
+
+$(".submenu5").hide(function () {
+});
+
+$(".menuli5").hover(function () {
+    $(".submenu5").slideDown();
+},
+function () {
+    $(".submenu5").hide();
+});
+
+$(".submenu5").hover(function () {
+    $(".submenu5").show();
+},
+function () {
+    $(".submenu5").hide();
+});
---
+++ MZ_webhf115/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MZ_webhf115.MvcApplication" Language="C#" %>
