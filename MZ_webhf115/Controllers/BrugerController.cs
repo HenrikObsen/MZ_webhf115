38943d3c8502432755cfa2d00b1853fc552c6f07
HenrikObsen <hto@djes.dk>
2015-12-08 12:46:08 +0100

    login

---
+++ MZ_webhf115/Controllers/BrugerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Security;
+using RepoMZ;
+
+namespace MZ_webhf115.Controllers
+{
+    public class BrugerController : Controller
+    {
+        BrugerFac brugerFac = new BrugerFac();
+        // GET: Bruger
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Email, string Password)
+        {
+            Bruger bruger = brugerFac.Login(Email.Trim(), Crypto.Hash(Password.Trim()));
+
+            if (bruger.ID > 0)
+            {
+                FormsAuthentication.SetAuthCookie(bruger.ID.ToString(),true);
+
+                Session["UserID"] = bruger.ID;
+                Session["Level"] = bruger.Level;
+                Session.Timeout = 120;
+                return Redirect("/Bruger/Secret/");
+            }
+            else
+            {
+                ViewBag.MSG = "Brugeren blev ikke fundet!!!";
+                return View("Index");
+            }
+
+
+        }
+        
+        [Authorize]
+        public ActionResult Secret()
+        {
+            return View();
+        }
+    }
+}
--- MZ_webhf115/Controllers/ProduktController.cs
+++ MZ_webhf115/Controllers/ProduktController.cs
@@ -8,9 +8,10 @@ namespace MZ_webhf115.Controllers
         ProduktFac prodFac = new ProduktFac();
 
         // GET: Produkt
-        public ActionResult ProduktListe(int id)
+        public ActionResult ProduktListe(int id=1)
         {
             return View(prodFac.GetProduktListe(id));
         }
     }
 }
+
